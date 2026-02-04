18d3f09780b9a403ceae57bb7e2321f509084bc6
edineievr <edney2322@gmail.com>
2026-02-03 22:00:28 -0300

    Implements initial infrastructure

---
+++ Sales.Infrastructure/Persistence/Mappings/OrderMapping.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Domain.Orders.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales.Infrastructure.Persistence.Mappings
+{
+    public class OrderMapping : IEntityTypeConfiguration<Order>
+    {
+
+    }
+}
---
+++ Sales.Infrastructure/Persistence/SalesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales.Infrastructure.Persistence
+{
+    public class SalesDbContext : DbContext
+    {
+        public SalesDbContext(DbContextOptions<SalesDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
