3024cf2dc3fda92168a87b6e8f578ba38aa1540c
edineievr <edney2322@gmail.com>
2026-02-04 09:36:00 -0300

    Add Order mapping in infrastructure layer

--- Sales.Infrastructure/Persistence/Mappings/OrderMapping.cs
+++ Sales.Infrastructure/Persistence/Mappings/OrderMapping.cs
@@ -3,11 +3,28 @@ using Sales.Domain.Orders.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sales.Infrastructure.Repositories;
 
 namespace Sales.Infrastructure.Persistence.Mappings
 {
     public class OrderMapping : IEntityTypeConfiguration<Order>
     {
-
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("orders");
+            builder.HasKey(order => order.Id);
+            builder.Property(order => order.Id).ValueGeneratedOnAdd();
+            
+            builder.Property(order => order.Status).IsRequired();
+            builder.Property(order => order.CreationDate).IsRequired();
+            builder.Property(order => order.InvoiceDate);
+            builder.Property(order => order.CancelationDate);
+            
+            builder.HasMany(order => order.Items)
+                   .WithOne()
+                   .HasForeignKey(item => item.OrderId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
