231f90983d362368f38e44c7567678864b78a7f2
edineievr <edney2322@gmail.com>
2026-02-04 10:06:45 -0300

    Add order item mapping in infrastructure layer

---
+++ Sales.Infrastructure/Persistence/Mappings/OrderItemMapping.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sales.Domain.Orders.Entities;
+
+namespace Sales.Infrastructure.Persistence.Mappings;
+
+public class OrderItemMapping : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.ToTable("orderitems");
+        builder.HasKey(item => item.Id);
+        builder.Property(item => item.Id).ValueGeneratedOnAdd();
+
+        builder.Property(item => item.OrderId).IsRequired();
+        builder.Property(item => item.ProductId).IsRequired();
+        builder.Property(item => item.Quantity)
+               .IsRequired()
+               .HasPrecision(10, 2);
+        builder.Property(item => item.UnitPrice).IsRequired();
+        builder.HasIndex(item => item.OrderId);
+    }
+}
