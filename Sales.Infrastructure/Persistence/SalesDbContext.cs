aa7bf2dd6b48a5fa5a2944b727b44565bce0be6e
edineievr <edney2322@gmail.com>
2026-02-04 10:07:43 -0300

    Configure model creating to scan assembly for mappings

--- Sales.Infrastructure/Persistence/SalesDbContext.cs
+++ Sales.Infrastructure/Persistence/SalesDbContext.cs
@@ -2,14 +2,23 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Sales.Infrastructure.Persistence.Mappings;
 
 namespace Sales.Infrastructure.Persistence
 {
     public class SalesDbContext : DbContext
     {
+
         public SalesDbContext(DbContextOptions<SalesDbContext> options) : base(options)
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(SalesDbContext).Assembly);//scale scanning assembly instead passing mapping in method
+        }
     }
 }
+
+
